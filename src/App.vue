466fae7d88ccda191af5bb5031a62d445749f2b5
malekayadi01 <malekayadi01@gmail.com>
2024-02-06 13:53:23 +0100

    v1

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,10 @@
       <router-view />
     </div>
   </template>
+
+  
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   
   <script>
   import '@/assets/css/global-styles.css';
