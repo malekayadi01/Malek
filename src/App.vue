1f9e482d72c3641ffbbb9429dd737f658f5d1029
malekayadi01 <malekayadi01@gmail.com>
2024-02-06 13:59:22 +0100

    final

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,55 @@
 <template>
-    <div id="app">
-      <nav class="navbar navbar-expand-lg navbar-light" :class="{ 'navbar-dark': darkMode, 'navbar-light': !darkMode }">
-        <a class="navbar-brand" href="#">Portfolio</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <router-link class="nav-link" to="/">Home <span class="sr-only"></span></router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/projects">Projects</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/skills">Compétences</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/ContactPage">Contact</router-link>
-            </li>
-          </ul>
-  
-          <button @click="toggleDarkMode" class="btn btn-outline-info my-2 my-sm-0">
-            <i :class="darkMode ? 'fas fa-sun' : 'fas fa-moon'"></i>
-          </button>
-          <form class="form-inline my-2 my-lg-0">
-            <div class="input-group"></div>
-          </form>
-        </div>
-      </nav>
-  
-      <router-view />
-    </div>
-  </template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light" :class="{ 'navbar-dark': darkMode, 'navbar-light': !darkMode }">
+      <a class="navbar-brand" href="#">Portfolio</a>
+      <button class="navbar-toggler" type="button" @click="toggleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-  
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  
-  <script>
-  import '@/assets/css/global-styles.css';
-  
-  export default {
-    data() {
-      return {
-        darkMode: false,
-      };
+      <div class="collapse navbar-collapse" :class="{ 'show': isNavbarOpen }" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only"></span></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/projects">Projects</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/skills">Compétences</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/ContactPage">Contact</router-link>
+          </li>
+        </ul>
+
+        <button @click="toggleDarkMode" class="btn btn-outline-info my-2 my-sm-0">
+          <i :class="darkMode ? 'fas fa-sun' : 'fas fa-moon'"></i>
+        </button>
+      </div>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+import '@/assets/css/global-styles.css';
+
+export default {
+  data() {
+    return {
+      darkMode: false,
+      isNavbarOpen: false
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+      document.body.classList.toggle('dark-mode', this.darkMode);
     },
-    methods: {
-      toggleDarkMode() {
-        this.darkMode = !this.darkMode;
-        document.body.classList.toggle('dark-mode', this.darkMode);
-      },
-    },
-  };
-  </script>
-  
+    toggleNavbar() {
+      this.isNavbarOpen = !this.isNavbarOpen;
+    }
+  },
+};
+</script>
